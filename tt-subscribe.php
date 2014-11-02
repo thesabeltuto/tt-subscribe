52ac214e760d8052dc556ebd62b6a6842548d4ce
Thesabel Tuto <thesabeltuto@gmail.com>
2014-11-02 12:28:03 +0800

    Version 4.0

--- tt-subscribe.php
+++ tt-subscribe.php
@@ -1,13 +1,16 @@
 <?php
 /*
 	Plugin Name: TT - Subscribe
-	Plugin URI: http://ttplugins.wordpress.com/tt-subscribe
-	Description: This plugin is a simplified subscription box that is totally customizable. It currently includes facebook, twitter and feedburner. Other social media links will be included soon. Created by Thesabel Tuto.
-	Version: 1.0
+	Plugin URI: http://wordpress.org/extend/plugins/tt-subscribe/
+	Description: This plugin is a simplified subscription box that supports unlimited customization. It currently includes facebook, twitter, google plus and feedburner. Wigetized, hooked, full control and total customization, mobile friendly, responsive, new dashboard and many more features!
+	Version: 4.0
 	Author: Thesabel Tuto
 	Author URI: http://thesabeltuto.blogspot.com
+	Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=H228JQZP6269J&lc=PH&item_name=TT%2dPlugins&item_number=tt%2dplugins¤cy_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
+	License: GPLv2 or later
+	License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-	Copyright 2013  THESABEL UY TUTO, CSNA, MBA  (email : thesabeltuto@gmail.com)
+	Copyright 2014  THESABEL UY TUTO, CSNA, MBA  (email : thesabeltuto@gmail.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as 
@@ -23,75 +26,35 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-define('TT_SUBSCRIBE_VERSION', '1.0');
-
 // Define paths and variables
-define('TT_SUBSCRIBE_PATH', dirname(__FILE__));
-define('TT_SUBSCRIBE_FOLDER', basename(dirname(__FILE__)));
-define('TT_SUBSCRIBE_ABSPATH', trailingslashit(str_replace('\\','/', WP_PLUGIN_DIR.'/'.TT_SUBSCRIBE_FOLDER)));
-define('TT_SUBSCRIBE_URLPATH', trailingslashit(plugins_url(TT_SUBSCRIBE_FOLDER)));
+define('TT_SUBSCRIBE_FILE', __FILE__ );
+define('TT_SUBSCRIBE_DIR', plugin_dir_path(__FILE__));
+define('TT_SUBSCRIBE_URL', plugin_dir_url(__FILE__));
 
-require_once TT_SUBSCRIBE_PATH . '/install.php';
+// Include all files
+require_once(TT_SUBSCRIBE_DIR.'includes/install.php');
+require_once(TT_SUBSCRIBE_DIR.'includes/widget.php'); /* fix on sidebar area */
 
-if ( is_admin() )
-	require_once TT_SUBSCRIBE_PATH . '/admin.php';
-	
-
-function tt_subscribe() {
-	wp_enqueue_script('jquery.css', 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js');
-    wp_enqueue_script('script.js', TT_SUBSCRIBE_URLPATH.'scripts/script.js');
-    wp_enqueue_style('style.css', TT_SUBSCRIBE_URLPATH.'styles/style.css');
-
-?>
-
-
-	<style>
-	div#tt-subscribe, #tt-subscribe-icons { width: <?php echo get_option('tt-subscribe-width'); ?>px!important; }
-    #tt-subscribe-header { background: #<?php echo get_option('tt-subscribe-background'); ?>!important; }
-	#tt-subscribe-area-facebook, #tt-subscribe-area-twitter, #tt-subscribe-area-feedburner {
-		 width: <?php echo get_option('tt-subscribe-width')-20; ?>px!important;
-	}
-    </style>
-
-<div id="tt-subscribe">
-	<div id="tt-subscribe-header">
-        <h1><span id="h1up">GET OUR TOP STORIES</span><br />FOLLOW <?php bloginfo( $show ); ?></h1>
-        <div id="tt-subscribe-icons">
-            <div id="tt-subscribe-facebook"></div>
-            <div id="tt-subscribe-twitter"></div>
-            <div id="tt-subscribe-feedburner"></div>
-        </div>
-    </div>
-    <div id="tt-subscribe-area">
-        <div id="tt-subscribe-area-facebook">
-            <div id="fb-root"></div>
-            <script>(function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) return;
-              js = d.createElement(s); js.id = id;
-              js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-              fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));</script>
-            <div class="fb-like" data-href="<?php echo get_option('tt-subscribe-facebook-url'); ?>" data-send="false" data-width="<?php echo get_option('tt-subscribe-width')-20; ?>" data-show-faces="true" data-font="arial"></div>
-        	<a href="<?php echo get_option('tt-subscribe-facebook-url'); ?>" alt="<?php echo get_option('tt-subscribe-facebook-txt'); ?>"><?php echo get_option('tt-subscribe-facebook-txt'); ?></a>
-        </div>
-        <div id="tt-subscribe-area-twitter">
-            <a href="<?php echo get_option('tt-subscribe-twitter-url'); ?>" class="twitter-follow-button" data-show-count="true" data-lang="en"><?php echo get_option('tt-subscribe-twitter-txt'); ?></a>
-            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>        
-        	<a href="<?php echo get_option('tt-subscribe-twitter-url'); ?>" alt="<?php echo get_option('tt-subscribe-twitter-txt'); ?>"><?php echo get_option('tt-subscribe-twitter-txt'); ?></a>
-        </div>
-        <div id="tt-subscribe-area-feedburner">
-     	   <a href="<?php echo get_option('tt-subscribe-feedburner-url'); ?>" alt="<?php echo get_option('tt-subscribe-feedburner-txt'); ?>"><?php echo get_option('tt-subscribe-feedburner-txt'); ?></a>
-        </div>
-
-    </div>
-</div>
-
-<?php
+// Load Plugin
+add_action('init', 'load_tt_subscribe_scripts');
+add_action('wp_head', 'load_tt_subscribe_custom');
+if ( is_admin() ) {
+	require_once(TT_SUBSCRIBE_DIR.'includes/admin.php');
+	add_action('admin_menu', 'tt_subscribe_admin_menu');
 }
-?>
 
+require_once(TT_SUBSCRIBE_DIR.'includes/defaults.php');
 
+// for hook
+add_action('tt_subscribe', 'tt_subscribe');
+add_shortcode('tt_subscribe', 'tt_subscribe'); /* fix on content area: post or page */
 
+function load_tt_subscribe_scripts() {
+	wp_enqueue_script('jquery');
+    wp_enqueue_script('tt_subscribe_script.js', TT_SUBSCRIBE_URL.'js/script.js');
+    wp_enqueue_style('tt_subscribe_style.css', TT_SUBSCRIBE_URL.'css/style.css');	
+}
 
-
+function load_tt_subscribe_custom() {
+	require_once(TT_SUBSCRIBE_DIR.'includes/custom-codes.php');
+}
